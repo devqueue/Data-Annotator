072fb1c5ec716721792e0d0175c7fb30e1ad184f
devqueue <haziq.sayyed@gmail.com>
2022-11-23 20:24:27 +0530

    export added

--- apps/search/urls.py
+++ apps/search/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.classification, name="classification"),
+    path('', views.classification_view, name="classification"),
     path('ner', views.ner, name="ner"),
     path('export', views.export, name="export"),
 ]
